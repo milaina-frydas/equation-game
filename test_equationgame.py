764bb051f1c02a70c34eb406783a8c306a0595f0
milainafrydas <milfry0809@highgateschool.org.uk>
2023-03-09 09:09:28 +0000

    main menu added

--- test_equationgame.py
+++ test_equationgame.py
@@ -54,3 +54,81 @@ class TestCompileUserEquation(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
+BEGIN
+running <- True
+WHILE running DO
+FOR event IN pygame.event.get() DO
+IF event.type == pygame.QUIT THEN
+running <- False
+END IF
+END FOR
+
+scss
+Copy code
+    background <- pygame.image.load(default_image)
+
+    screen.blit(background, (0, 0))
+
+    # Title
+    title_font <- pygame.font.Font('EraserDust.ttf', 100)
+    title <- title_font.render('Equation Game', True, 'white')
+    screen.blit(title, (100, 50))
+
+    # Select Level
+    level_font <- pygame.font.Font('EraserDust.ttf', 75)
+    level_select_text <- level_font.render('Select Level:', True, 'white')
+    screen.blit(level_select_text, (200, 200))
+
+    button_font <- pygame.font.Font('EraserDust.ttf', 40)
+    level_1_button <- button_font.render('Level 1', True, 'white')
+    screen.blit(level_1_button, (350, 300))
+    level_2_button <- button_font.render('Level 2', True, 'white')
+    screen.blit(level_2_button, (350, 350))
+    level_3_button <- button_font.render('Level 3', True, 'white')
+    screen.blit(level_3_button, (350, 400))
+
+    mouse <- pygame.mouse.get_pos()
+    click <- pygame.mouse.get_pressed()
+
+    IF 520 + 10 > mouse[0] > 520 - 10 and 322 + 10 > mouse[1] > 322 - 10 THEN
+        pygame.draw.circle(screen, (191, 255, 128), (520, 322), 10.0, 0)
+        IF click[0] == 1 THEN
+            image <- 'chalkboard.jpg'
+            level <- 1
+            selected_nums <- []
+            game_round(image, level, selected_nums)
+            running <- False
+        END IF
+    ELSE
+        pygame.draw.circle(screen, (255, 153, 153) , (520, 322), 10.0, 0)
+    END IF
+
+    IF 520 + 10 > mouse[0] > 520 - 10 and 372 + 10 > mouse[1] > 372 - 10 THEN
+        pygame.draw.circle(screen, (191, 255, 128), (520, 372), 10.0, 0)
+        IF click[0] == 1 THEN
+            image <- 'chalkboard.jpg'
+            level <- 2
+            selected_nums <- []
+            game_round(image, level, selected_nums)
+            running <- False
+        END IF
+    ELSE
+        pygame.draw.circle(screen, (255, 153, 153) , (520, 372), 10.0, 0)
+    END IF
+
+    IF 520 + 10 > mouse[0] > 520 - 10 and 422 + 10 > mouse[1] > 422 - 10 THEN
+        pygame.draw.circle(screen, (191, 255, 128), (520, 422), 10.0, 0)
+        IF click[0] == 1 THEN
+            image <- 'chalkboard.jpg'
+            level <- 3
+            selected_nums <- []
+            game_round(image, level, selected_nums)
+            running <- False
+        END IF
+    ELSE
+        pygame.draw.circle(screen, (255, 153, 153) , (520, 422), 10.0, 0)
+    END IF
+
+    pygame.display.update()
+END WHILE
+END
